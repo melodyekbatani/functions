141c8258d709edfa68b0b0f8859f380d0d0dd7fa
melodyekbatani <melodyekbatani@gmail.com>
2026-04-01 13:03:48 -0400

    Content from API

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,39 @@
+// Function to render your items.
 let renderItems = (data) => {
-    console.log(data)
+	// The `ul` where the items will be inserted.
+	let dataList = document.getElementById('data-list')
+
+	// Loop through each item in the data array:
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+	data.forEach((item) => {
+		let conditionalClass = '' // Set an empty class variable.
+
+		// Conditional if this is `false` (“not true”):
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+		if (!item.alsoWrote) {
+			conditionalClass = 'faded' // Update the variable.
+		}
+
+		// Make a “template literal” as we have before, inserting your data (and maybe the class):
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+		let listItem =
+			`
+				<li class="${conditionalClass}">
+					<h2>${item.Name}</h2>
+					<p>Released in <time>${item.Category}</time></p>
+                    <img src="${item.Image}">
+					<p><em>${item.Personality}</em></p>
+					<a href="${item.Linktoitem}">
+					</a>
+				</li>
+			`
+
+		dataList.insertAdjacentHTML('beforeend', listItem) // Add it to the `ul`!
+
+		// Don’t feel limited to `ul > li` for these—you can insert any DOM, anywhere!
+	})
 }
+
 // Fetch gets your (local) JSON file…
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 fetch('assets/data.json')
