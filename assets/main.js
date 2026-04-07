38d512351d12bd834d8ccc0aa37668221cfba18b
melodyekbatani <melodyekbatani@gmail.com>
2026-04-07 11:52:46 -0400

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -5,7 +5,8 @@ let renderItems = (data) => {
 	data.forEach((item) => {
 		let containerEl = document.getElementById(`${item['Category'].toLowerCase()}-list`)
 		let itemHtml =`
-		<li>
+		<li data-category="${item['Category']}" data-vibes="${item['Vibes']}" class='active'>
+			
 			<h2>${item['Name']}</h2>
 			<img src="${item['Image']}">
 		</li>
@@ -15,9 +16,22 @@ let renderItems = (data) => {
 		containerEl.insertAdjacentHTML('beforeend', itemHtml)
 	})
 }
+// Creating function to set up the distribution of selected items 
+let getAverages =(data) => {
+	let Averages = {}
+	for (let item of data) {
+		if (key in Averages) {Averages[key] += 1} 
+		else {Average[key] = 1;}
+	}
+// Introducing hashmaps - set of keys and values, first thing we do - is say have we seen the keys before(vibes - balanced/unhinged) - keep track of the number of items it has seen 
+}
+
+
 fetch('assets/data.json')
 	.then(response => response.json())
 	.then(data => {
 		// And passes the data to the function, above!
 		renderItems(data)
+		let selected = document.getElementsByClassName('active')
+	getAverages(selected);
 	})
