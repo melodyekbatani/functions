0d4e3b3b097fae55f1a6a9551413e77efe40271a
melodyekbatani <melodyekbatani@gmail.com>
2026-04-07 01:21:06 -0400

    basic styling

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,17 @@
 // Function to render your items.
 // I started by customizing the Json file from Eric's lecture then reviewed this with the tutor and customized the basic format to fit within the 4 dropdowns I created in the HTML file. I'm setting up the dropdown menu then calling each child in this first part
+
 let renderItems = (data) => {
 	data.forEach((item) => {
-		let containerEl = document.getElementById('data-list')
+		let containerEl = document.getElementById(`${item['Category'].toLowerCase()}-list`)
 		let itemHtml =`
 		<li>
 			<h2>${item['Name']}</h2>
+			<img src="${item['Image']}">
 		</li>
 		`
+		// https://chatgpt.com/c/69d48e7e-b5c0-8332-b3b7-0280e0e87c6e
+
 		containerEl.insertAdjacentHTML('beforeend', itemHtml)
 	})
 }
