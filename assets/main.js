b884c2d771ac0006bc3c3c2b2183c02ea8d66223
melodyekbatani <melodyekbatani@gmail.com>
2026-04-17 23:16:21 -0400

    Adding code comments

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-// I begin by creating and setting global values, defining these variables at the beginning of the file which means they can be access below. A few key variables I used are currentPlate, foodItem for storing all items and resetting the selection when the user hits restart as well as the categories that are used throughout but specifically within the toggleNext button 
+// I begin by creating and setting global values. I'm defining these variables at the beginning of the file which means they can be access below. A few key variables I used are currentPlate, foodItem for storing all items and resetting the selection when the user hits restart as well as the categories that are used throughout but specifically within the toggleNext button 
 let currentPlate ={}
 let foodItem = []; 
 const categories = ['Base', 'Protein', 'Crunch', 'Sweet']
@@ -15,13 +15,13 @@ let renderItems = (data) => {
 		</li>
 		`
 
-// When the food items are clicked, it toggles an active state (styled with css) on that element indicating to the user the item has been selected. the This is the original LLM use that I shared, since learning this wasn't the correct way to prompt I went in revised the code so that I could understand it better and make the logic my own https://claude.ai/share/80671624-d8b8-4373-b790-a0a73f990823. To further understand this, I also reviewed this mdn link: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener. for reference, e = event, that on a click event, the code targets the element and toggles the active styling.
+// When the food items are clicked, it toggles an active state (styled with css) on that element indicating to the user the item has been selected. This is the original LLM use that I shared, since learning this wasn't the correct way to prompt I went in revised the code so that I could understand it better and make the logic my own https://claude.ai/share/80671624-d8b8-4373-b790-a0a73f990823. To further understand this, I also reviewed this mdn link: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener. for my reference, e = event, so on a click event, the code targets the element and toggles the active styling.
 
 		containerEl.insertAdjacentHTML('beforeend', itemHtml)
 		containerEl.lastElementChild.addEventListener("click", (e) => {e.currentTarget.classList.
 		toggle('active')
 
-//I created this to clearly indicate to a user that they must select at least one item before moving to the next modal. Every time a user select a food item, the toggleNextButton function runs, doing a querySelector and checking if li[data-category="Base”] active exists. If querySelector li[data-category="Base”] returns nothing selected, then it disable the next button, but if it returns an HTML element that has been selected, then enable the next button. It's important to note the sweets "create plate button" isn't in the same modals as the other buttons so I need to add the disabled/enable state to that directly.
+//I created this to clearly indicate to a user that they must select at least one item before moving to the next modal. Every time a user select a food item, the toggleNextButton function runs, doing a querySelector and checking if example li data-category="Base” active exists. If querySelector returns nothing selected, then it disable the next button, but if it returns an HTML element that has been selected, then enable the next button. It's important to note the sweets "create plate button" isn't in the same class as the other buttons so I need to add the disabled/enable state to that directly.
 
 			if (currentModal <= 4) {
 				toggleNextButton(currentModal - 1)
@@ -43,6 +43,7 @@ function getAverages (data) {
 	}
 	return Averages
 }
+
 //This function takes the average and converts each vibe count into a percent. for loop is going through ever property in the data and loops through each key, so for example chaotic: 2, girlcore: 1, balanced: 1 - sum adds it all up. Percent takes each and divides it by the total to create the most dominant category. The sum variable loops through each property and counts to add up the total count of the vibes. Then I loop through again and divide each vibe by the total to get percentage. Then add it to the HTML so it can show the vibe name. 
 function getPercent (data) {
 	let Percent = {}
@@ -59,7 +60,9 @@ function getPercent (data) {
 	}
 	return html
 }
-// working with claude to implement the output options https://claude.ai/share/80671624-d8b8-4373-b790-a0a73f990823 
+// Here I'm defining the most dominant category by creating and if statement. starting at 0 because no vibe is chosen, then keeping track of the vibe that's being picked the most times. holds the name of the vibe and returns it back to the user.
+// 
+
 function getMDC (Averages) {
 	let mdc = null
 	let highestCount = 0
@@ -72,9 +75,10 @@ function getMDC (Averages) {
 	return mdc
 }
 
-// random x array length (less than 1 and in between 0-1) getting random item from selected items
+// This function runs when the button create plate button is clicked is clicked defines a random x array length (less than 1 and in between 0-1) if the user doesn't select an item in the most dominant category.
 const getRandomItem = (arr) =>
 	arr[Math.floor(Math.random() * arr.length)]
+
 
 const createPlate = document.getElementById('create-plate')
 createPlate.addEventListener('click', () => {
